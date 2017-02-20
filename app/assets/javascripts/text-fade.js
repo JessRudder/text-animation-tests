efbb708458ea13d5e30f88dfad12470a91faf37f
JessRudder <thejessicarudder@gmail.com>
2017-02-19 21:48:41 -0500

    Solve issue with animate class only removing from first name element.

--- app/assets/javascripts/text-fade.js
+++ app/assets/javascripts/text-fade.js
@@ -12,7 +12,7 @@ var controller = new ScrollMagic.Controller();
 // .addTo(controller);
 
 // Scene Handler
-var animateElem = document.getElementsByClassName("text-fade-animation");
+var $animateElem = $('.text-fade-animation');
 
 var scene1 = new ScrollMagic.Scene({
   triggerElement: "#pinned-trigger1", // point of execution
@@ -22,18 +22,17 @@ var scene1 = new ScrollMagic.Scene({
 })
   .setPin("#pinned-element1") // the element we want to pin
   .on("enter", function() {
-    [].forEach.call(animateElem, function (el) {
-        el.style.opacity = 0;
-        setTimeout(function() {
-          el.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jessica&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-          el.style.opacity = 1;
-        }.bind(null,el),1750)
-    });
+    $animateElem.css('opacity', '0');
+    setTimeout(function() {
+      var text = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jessica&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+      $animateElem.each(function(idx,el){
+        el.innerHTML = text;
+      });
+      $animateElem.css('opacity','1');
+    },1750)
 
     setTimeout(function() {
-      [].forEach.call(animateElem, function (el) {
-        el.classList.remove("text-fade-animation");
-      });
+      $animateElem.removeClass('text-fade-animation')
     },3750)
   })
   .addTo(controller);
