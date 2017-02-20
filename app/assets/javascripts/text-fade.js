a37ef115eb2921cf4acd6da9dca090cd5d05d110
JessRudder <thejessicarudder@gmail.com>
2017-02-19 20:27:40 -0500

    Add first screen. Get animation in place for Michaelangelo.

--- app/assets/javascripts/text-fade.js
+++ app/assets/javascripts/text-fade.js
@@ -14,9 +14,35 @@ var controller = new ScrollMagic.Controller();
 // Scene Handler
 var scene1 = new ScrollMagic.Scene({
   triggerElement: "#pinned-trigger1", // point of execution
-  duration: 150, // pin element for the window height - 1
+  duration: $(window).height(), // pin element for the window height - 1
   triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
   reverse: true // allows the effect to trigger when scrolled in the reverse direction
 })
-.setPin("#pinned-element1") // the element we want to pin
-.addTo(controller);
+  .setPin("#pinned-element1") // the element we want to pin
+  .addTo(controller);
+
+var animateElem = document.getElementsByClassName("text-fade-animation");
+
+var scene2 = new ScrollMagic.Scene({
+  triggerElement: "#anim-trigger1",
+  offset: 40,
+  duration: 300
+})
+  .on("enter", function() {
+    [].forEach.call(animateElem, function (el) {
+        // debugger;
+        el.style.opacity = 0;
+    });
+    
+
+  })
+  .on("leave", function() {
+    [].forEach.call(animateElem, function (el) {
+        el.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Jessica&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+        el.style.opacity = 1;
+        setTimeout(function() {
+          el.classList.remove("text-fade-animation")
+        }.bind(null,el),2000);
+    });
+  })
+  .addTo(controller)
